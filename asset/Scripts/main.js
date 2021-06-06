71722cf5c85cb9ff3b09caf9320fe169672e20ba
Damilola7717 <damilola7717@gmail.com>
2021-06-06 07:15:58 +0100

    Complete Investor Ui, Forgot password Ui except payout Ui

--- asset/Scripts/main.js
+++ asset/Scripts/main.js
@@ -8,5 +8,46 @@ $(document).ready(function(){
 		$('#addFarmModal').modal({
       backdrop: 'static'
 		});
+	});
+	$('.reveal').on('click', function(){
+		var $pwd = $('.pwd');
+		if($pwd.attr('type') === 'password'){
+			$pwd.attr('type', 'text');
+		} else{
+			$pwd.attr('type', 'password')
+		}
 	}); 
+	$('.reveal1').on('click', function(){
+		var $pwd = $('.pwd1');
+		if($pwd.attr('type') === 'password'){
+			$pwd.attr('type', 'text');
+		} else{
+			$pwd.attr('type', 'password')
+		}
+	}); 
+	$('.reveal2').on('click', function(){
+		var $pwd = $('.pwd2');
+		if($pwd.attr('type') === 'password'){
+			$pwd.attr('type', 'text');
+		} else{
+			$pwd.attr('type', 'password')
+		}
+	}); 
+	$('.reveal3').on('click', function(){
+		var $pwd = $('.pwd3');
+		if($pwd.attr('type') === 'password'){
+			$pwd.attr('type', 'text');
+		} else{
+			$pwd.attr('type', 'password')
+		}
+	}); 
+	$('.reveal4').on('click', function(){
+		var $pwd = $('.pwd4');
+		if($pwd.attr('type') === 'password'){
+			$pwd.attr('type', 'text');
+		} else{
+			$pwd.attr('type', 'password')
+		}
+	}); 
+	
 });
